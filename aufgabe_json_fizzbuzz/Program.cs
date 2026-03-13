da15f0cd4818bc382961f3f9a14a41c0901bd16f
Deniz Tugrul <deniztugrul@protonmail.com>
2026-03-13 10:28:11 +0100

    Initial commit: add JSON-driven FizzBuzz console application

---
+++ aufgabe_json_fizzbuzz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using System.IO;
+
+namespace aufgabe_json_fizzbuzz;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Geben Sie eine json-Datei an. Lassen Sie das Feld leer für eine normale Zahlenausgabe.");
+        string jsonFileInput = Console.ReadLine();
+        Console.WriteLine();
+        
+        if (jsonFileInput == "")
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        else if (File.Exists(jsonFileInput))
+        {
+            string json = File.ReadAllText(jsonFileInput);
+            List<Rule> rules = JsonSerializer.Deserialize<List<Rule>>(json);
+
+            for (int i = 1; i < 101; i++)
+            {
+                string result = "";
+                
+                foreach (Rule rule in rules)
+                {
+                    if (i % rule.Value == 0)
+                    {
+                        result += rule.Output;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(result))
+                    Console.WriteLine(i);
+
+                else
+                    Console.WriteLine(result);
+            }
+        }
+        else
+            Console.WriteLine("\nDie angegebene Datei existiert nicht.");
+    }
+}
---
+++ aufgabe_json_fizzbuzz/Rule.cs
@@ -0,0 +1,7 @@
+namespace aufgabe_json_fizzbuzz;
+
+public class Rule
+{
+    public int Value { set; get; }
+    public string Output { set; get; }
+}
